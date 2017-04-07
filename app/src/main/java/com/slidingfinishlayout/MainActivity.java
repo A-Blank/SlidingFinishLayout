ed2f8119fd7fcf38f482349cdde5b65038214a9d
A-Blank <954203532@qq.com>
2017-04-07 12:56:00 +0800

    Initial commit

---
+++ app/src/main/java/com/slidingfinishlayout/MainActivity.java
@@ -0,0 +1,25 @@
+package com.slidingfinishlayout;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity implements SlidingFinishView.SlidingFinishCallback{
+
+    private SlidingFinishView view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        view= (SlidingFinishView) findViewById(R.id.activity_main);
+//        view.setSlideEnable(false);
+//        view.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    public void onFinish() {
+        finish();
+    }
+}
